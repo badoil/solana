58613020bcc4c9025061ed34c220e76bb064ea6e
oneal.oh <oneal.oh@kakaomobility.com>
2022-05-04 21:22:13 +0900

    update: lib.rs, struct PostAccount

--- fb/programs/fb/src/lib.rs
+++ fb/programs/fb/src/lib.rs
@@ -13,3 +13,21 @@ pub mod fb {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+// post account structure
+pub struct PostAccount {
+    pub authority: Pubkey,
+
+    pub text: String,
+
+    pub poster_name: String,
+
+    pub poster_url: String,
+
+    pub comment_count: u64, 
+
+    pub index: u64,
+
+    pub post_time: i64,
+}
