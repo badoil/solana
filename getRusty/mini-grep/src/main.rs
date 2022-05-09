3ed7a914bf7cfc5e871dee0f4f6ff5c117cbf590
oneal.oh <oneal.oh@kakaomobility.com>
2022-05-09 22:17:28 +0900

    fix: mini-grep, main.rs refactoring2

--- getRusty/mini-grep/src/main.rs
+++ getRusty/mini-grep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::parse_config(&args);
 
     println!("query: {}", config.query);
     println!("filename: {}", config.filename);
@@ -20,9 +20,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn parse_config(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
 }
