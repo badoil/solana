a5cb8f34180dd1c1a5877642da5b63717252b42a
oneal.oh <oneal.oh@kakaomobility.com>
2022-05-09 22:23:46 +0900

    fix: mini-grep, main.rs refactoring3

--- getRusty/mini-grep/src/main.rs
+++ getRusty/mini-grep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+);
 
     println!("query: {}", config.query);
     println!("filename: {}", config.filename);
@@ -21,7 +25,10 @@ struct Config {
 }
 
 impl Config {
-    fn parse_config(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
     
